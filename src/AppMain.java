000d2e441477c1edbba420bd6f54a0c1373cc759
William Huang <shsidforever@gmail.com>
2017-11-13 23:15:41 +0800

    created the beginning of the swing gui

---
+++ src/AppMain.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class AppMain {
+    public static void main(String[] args) {
+        String input = "C:\\Users\\willi\\Documents\\Projects\\Commission Calculator\\src\\October.xls";
+        String output = "C:\\Users\\willi\\Documents\\Projects\\Commission Calculator\\src\\output.xls";
+        File fin = new File(input);
+        File fout = new File(output);
+        ExcelReader main = new ExcelReader(fin,fout);
+        main.run();
+
+    }
+}
