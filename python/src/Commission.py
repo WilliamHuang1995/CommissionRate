f1d9074b9700f0833c14fc4879b7fb42c517b762
William Huang <shsidforever@gmail.com>
2017-12-26 21:48:24 +0800

    updated check statements. much clearer now

--- python/src/Commission.py
+++ python/src/Commission.py
@@ -1,6 +1,7 @@
-import openpyxl
 from tkinter import *
 from tkinter import filedialog
+import openpyxl
+import getpass
 
 
 class GUI:
@@ -14,12 +15,13 @@ class GUI:
         self.output_button = Button(master, text="Select Output", command=self.select_output).grid(row=1)
         self.output_label = Label(master, text="", anchor="w", width=50)
         self.output_label.grid(row=1, column=1)
-        self.run_button = Button(master, text="Run!", command=main).grid(row=2)
+        self.run_button = Button(master, text="Run Program!", command=main).grid(row=2)
         self.close_button = Button(master, text="Close", command=master.quit).grid(row=2, column=1)
 
     def select_input(self):
         global path
-        fn = filedialog.askopenfilename(initialdir="/", title="Select file",
+        user = getpass.getuser()
+        fn = filedialog.askopenfilename(initialdir="C:/Users/%s/Documents" % user, title="Select file",
                                         filetypes=(("Excel 2010 Files", "*.xlsx"), ("All Files", "*.*")))
         self.input_label['text'] = fn
         path = self.input_label['text']
@@ -51,6 +53,7 @@ def main():
     # remove default sheet that Workbook creates
     owb.remove_sheet(owb.get_sheet_by_name('Sheet'))
     owb.save(filename=dest)
+    # when successfully executed
     my_gui.output_label['text'] = 'Success'
     my_gui.input_label['text'] = 'Success'
 
@@ -58,30 +61,29 @@ def main():
 def process_data(ws):
     employee_dict = {}
     # loop through row and columns
-    for row in ws.iter_rows('A{}:J{}'.format(ws.min_row + 1, ws.max_row)):
+    for row in ws['A{}:J{}'.format(ws.min_row + 1, ws.max_row)]:
         employee = row[0].value.title()
         customer = row[2].value
         product = row[4].value
         revenue = row[9].value or 0
-        # check if contain keywords
+        # check if contain keywords/ put this in config file later
         if any(p in product for p in ('硬化劑', '發泡劑', '設備', '薄膜')):
             # check if employee already added
-            # if not any(emp for emp in employee_dict if emp == employee):
             if employee not in employee_dict.keys():
-                employee_dict[employee] = [{customer: {product: revenue}}]
+                employee_dict[employee] = {customer: {product: revenue}}
             else:
                 # check if customer already added
-                sales_report = employee_dict[employee]
+                customers_dict = employee_dict[employee]
                 # if there is no existing customer
-                if not any(cus for cus in sales_report if customer in cus):
-                    sales_report.append({customer: {product: revenue}})
+                if customer not in customers_dict.keys():
+                    customers_dict.update({customer: {product: revenue}})
                 else:
-                    cus = next(cus for cus in sales_report if customer in cus)
+                    products_dict = customers_dict[customer]
                     # if there is no existing product
-                    if not any(prod for prod in cus.get(customer) if product == prod):
-                        cus[customer][product] = revenue
+                    if product not in products_dict.keys():
+                        products_dict[product] = revenue
                     else:
-                        cus[customer][product] += revenue
+                        products_dict[product] += revenue
     return employee_dict
 
 
@@ -97,23 +99,22 @@ def output_data(dictionary, owb):
         # row starts at 2
         # column starts at 2
         row = 2
-
         # iterate thru companies
-        for companies in dictionary[employee]:
+        for company in dictionary[employee]:
             col = 3
-            for company in companies:
-                ows.cell(row=row, column=2).value = company
-                for product in companies[company]:
-                    revenue = companies[company][product]
-                    values = [product, revenue, revenue * 0.02, revenue * 0.03, revenue * 0.05]
-                    for i in range(len(values)):
-                        ows.cell(row=row, column=col + i).value = values[i]
-                    row += 1
+            ows.cell(row=row, column=2).value = company
+            for product in dictionary[employee][company]:
+                revenue = dictionary[employee][company][product]
+                values = [product, revenue, revenue * 0.02, revenue * 0.03, revenue * 0.05]
+                for i in range(len(values)):
+                    ows.cell(row=row, column=col + i).value = values[i]
+                row += 1
 
 
-path = ''
-dest = ''
-root = Tk()
-my_gui = GUI(root)
-root.mainloop()
-root.destroy()
+if __name__ == '__main__':
+    path = ''
+    dest = ''
+    root = Tk()
+    my_gui = GUI(root)
+    root.mainloop()
+    root.destroy()
